7e36d12eea1c51f6bdf4cd05759bcf6ef5a63eac
Diego Munguía Molina <6787861+dmunguiatec@users.noreply.github.com>
2021-02-08 18:26:33 -0600

    Ic 3002/cap11 (#2)
    * WIP: c11 programacion concurrente
    
    * WIP: IC-3002 c11 prueba de primalidad
    
    * IC-3002 c11 completando capitulo
    
    * IC-3002 c11 agregando imagen svg
    
    * Capitulo 11 concurrencia
    
    Co-authored-by: Diego Munguía <diego.munguia@edify.cr>

--- IC-3002/concurrencia/python/primalidad.py
+++ IC-3002/concurrencia/python/primalidad.py
@@ -1,6 +1,7 @@
 from timeit import default_timer as timer
 from multiprocessing import Process
 from os import getpid
+from multiprocessing import Value
 
 def es_primo_secuencial(n):
     for i in range(2, n):
@@ -8,58 +9,16 @@ def es_primo_secuencial(n):
             return False
     return True
 
+'''
 start = timer()
 print(f'¿es primo 433494437? {es_primo_secuencial(433494437)}') # 1073676287
 end = timer()
 print(f'Secuencial: {end - start} seg')
-
-def es_primo_concurrente(n):
-    
-    def aux(n, inicio, fin):
-        print(f'pid-{getpid()} Probando desde {inicio} hasta {fin}')
-
-        for i in range(inicio, fin):
-            if n % i == 0:
-                print(f'pid-{getpid()} False')
-                return
-        print(f'pid-{getpid()} True')
-
-    tam_lote = (n - 2) // 4
-    inicio = 2
-    fin = inicio + tam_lote
-    p1 = Process(target=aux, args=(n, inicio, fin))
-    p1.start()
-        
-    inicio = fin
-    fin = inicio + tam_lote
-    p2 = Process(target=aux, args=(n, inicio, fin))
-    p2.start()
-
-    inicio = fin
-    fin = inicio + tam_lote
-    p3 = Process(target=aux, args=(n, inicio, fin))
-    p3.start()
-
-    inicio = fin
-    fin = n
-    p4 = Process(target=aux, args=(n, inicio, fin))
-    p4.start()
-
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-
-start = timer()
-es_primo_concurrente(433494437)
-end = timer()
-print(f'Concurrente: {end - start} seg')
-    
-from multiprocessing import Value
-
+'''
+   
 def es_primo_concurrente_memc(n):
     
-    def aux(n, inicio, fin, res):
+    def tarea(n, inicio, fin, res):
         print(f'pid-{getpid()} Probando desde {inicio} hasta {fin}')
 
         for i in range(inicio, fin):
@@ -71,25 +30,25 @@ def es_primo_concurrente_memc(n):
     inicio = 2
     fin = inicio + tam_lote
     r1 = Value('b', False)
-    p1 = Process(target=aux, args=(n, inicio, fin, r1))
+    p1 = Process(target=tarea, args=(n, inicio, fin, r1))
     p1.start()
         
     inicio = fin
     fin = inicio + tam_lote
     r2 = Value('b', False)
-    p2 = Process(target=aux, args=(n, inicio, fin, r2))
+    p2 = Process(target=tarea, args=(n, inicio, fin, r2))
     p2.start()
 
     inicio = fin
     fin = inicio + tam_lote
     r3 = Value('b', False)
-    p3 = Process(target=aux, args=(n, inicio, fin, r3))
+    p3 = Process(target=tarea, args=(n, inicio, fin, r3))
     p3.start()
 
     inicio = fin
     fin = n
     r4 = Value('b', False)
-    p4 = Process(target=aux, args=(n, inicio, fin, r4))
+    p4 = Process(target=tarea, args=(n, inicio, fin, r4))
     p4.start()
 
     p1.join()
