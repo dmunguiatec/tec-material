23e907b43c510545a937b6beae031f9f7b891775
Diego Munguía <diego.munguia@edify.cr>
2021-02-15 20:40:28 -0600

    script para generar pdfs

---
+++ IC-3002/notas/render.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+pandoc a01.md  -o a01.pdf &&
+pandoc a02.md  -o a02.pdf &&
+pandoc c01.md  -o c01.pdf &&
+pandoc c02.md  -o c02.pdf &&
+pandoc c03.md  -o c03.pdf &&
+pandoc c04.md  -o c04.pdf &&
+pandoc c05.md  -o c05.pdf &&
+pandoc c06.md  -o c06.pdf &&
+pandoc c07.md  -o c07.pdf &&
+pandoc c08.md  -o c08.pdf &&
+pandoc c09.md  -o c09.pdf &&
+pandoc c10.md  -o c10.pdf &&
+pandoc c11.md  -o c11.pdf &&
+pandoc c12.md  -o c12.pdf &&
+pandoc intro.md  -o intro.pdf
